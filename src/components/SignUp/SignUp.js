12a6210723010397d6939e357a6fe2198927e8fa
Lim Yu De <lim.yude1@gmail.com>
2025-04-08 22:40:30 +0800

    fixed signup page

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -10,7 +10,7 @@ const SignUp = () => {
     username: "",
     email: "",
     password: "",
-    role: "admin",
+    role: "client",
   });
 
   const [error, setError] = useState(""); // State to manage error messages
