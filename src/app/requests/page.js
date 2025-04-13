e5332db1a0875053cf52055df46227da257f82bc
zackeus <zchoo2001@gmail.com>
2025-04-13 22:34:14 +0800

    Modified UI for no job bids and brute force access

--- src/app/requests/page.js
+++ src/app/requests/page.js
@@ -4,7 +4,6 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
-import { useStateManager } from "react-select";
 
 export default function Requests() {
   const router = useRouter();
